f9a7d754b85fabd5fed9183734f71eab90e9c183
Leo Westebbe <lw@Leos-MacBook-Pro.local>
2024-09-20 18:59:19 -0400

    add components for sidenav

--- components/ui/dashboard/nav-links.tsx
+++ components/ui/dashboard/nav-links.tsx
@@ -9,22 +9,18 @@ import {
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import { useTheme } from "next-themes";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: "Home", href: "/protected", icon: HomeIcon },
   { name: "Trips", href: "/protected/trips", icon: GlobeEuropeAfricaIcon },
-  {
-    name: "Invoices",
-    href: "/dashboard/invoices",
-    icon: DocumentDuplicateIcon,
-  },
-  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
   const pathname = usePathname();
+  const { theme } = useTheme();
 
   return (
     <>
@@ -35,9 +31,12 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-background p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
                 "bg-sky-100 text-blue-600": pathname === link.href,
+                "bg-slate-800 text-inherit":
+                  pathname === link.href && theme === "dark",
+                "hover:bg-slate-800 hover:text-inherit": theme === "dark",
               }
             )}
           >

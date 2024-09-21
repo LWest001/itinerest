86558162bc2ddce1af874ac3da1c7495fd91995c
Leo Westebbe <lw@Leos-MacBook-Pro.local>
2024-09-21 17:13:36 -0400

    fix edittrip by making it default export

--- app/protected/trips/[id]/edit/page.tsx
+++ app/protected/trips/[id]/edit/page.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import { Trip } from "@/global.types";
 import { getTripById } from "@/lib/data";
 import { Input } from "@/components/ui/input";
@@ -7,59 +5,63 @@ import { updateTrip } from "@/app/actions";
 import { Submit } from "@/components/ui/submit";
 import { Label } from "@/components/ui/label";
 
-export async function EditTrip({ params }: { params: { id: Trip["id"] } }) {
+type Props = {
+  params: {
+    id: Trip["id"];
+  };
+};
+
+export default async function EditTrip({ params }: Props) {
   const { id } = params;
   const trip = await getTripById(id);
   const updateTripWithId = updateTrip.bind(null, id);
 
   return (
-    <>
-      <form
-        className="flex w-full max-w-sm items-center gap-5 flex-col"
-        action={updateTripWithId}
-      >
-        <div className="w-full">
-          <Label htmlFor="name">Trip name</Label>
-          <Input type="text" defaultValue={trip?.name || ""} name="name" />
-        </div>
-        <div className="w-full">
-          <Label htmlFor="city">Trip location</Label>
-          <Input
-            type="text"
-            defaultValue={trip?.city || ""}
-            placeholder="City"
-            name="city"
-          />
-        </div>
-        <div className="w-full">
-          <Label htmlFor="lodging_name">Lodging</Label>
-          <Input
-            type="text"
-            defaultValue={trip?.lodging_name || ""}
-            placeholder="Where are you staying?"
-            name="lodging_name"
-          />
-        </div>
-        <div className="w-full">
-          <Label htmlFor="start_date">Start date</Label>
-          <Input
-            type="date"
-            defaultValue={trip?.start_date || ""}
-            placeholder="start date"
-            name="start_date"
-          />
-        </div>
-        <div className="w-full">
-          <Label htmlFor="end_date">End date</Label>
-          <Input
-            type="date"
-            defaultValue={trip?.end_date || ""}
-            placeholder="end date"
-            name="end_date"
-          />
-        </div>
-        <Submit type="submit">Save trip</Submit>
-      </form>
-    </>
+    <form
+      className="flex w-full max-w-sm items-center gap-5 flex-col"
+      action={updateTripWithId}
+    >
+      <div className="w-full">
+        <Label htmlFor="name">Trip name</Label>
+        <Input type="text" defaultValue={trip?.name || ""} name="name" />
+      </div>
+      <div className="w-full">
+        <Label htmlFor="city">Trip location</Label>
+        <Input
+          type="text"
+          defaultValue={trip?.city || ""}
+          placeholder="City"
+          name="city"
+        />
+      </div>
+      <div className="w-full">
+        <Label htmlFor="lodging_name">Lodging</Label>
+        <Input
+          type="text"
+          defaultValue={trip?.lodging_name || ""}
+          placeholder="Where are you staying?"
+          name="lodging_name"
+        />
+      </div>
+      <div className="w-full">
+        <Label htmlFor="start_date">Start date</Label>
+        <Input
+          type="date"
+          defaultValue={trip?.start_date || ""}
+          placeholder="start date"
+          name="start_date"
+        />
+      </div>
+      <div className="w-full">
+        <Label htmlFor="end_date">End date</Label>
+        <Input
+          type="date"
+          defaultValue={trip?.end_date || ""}
+          placeholder="end date"
+          name="end_date"
+        />
+      </div>
+      <Submit type="submit">Save trip</Submit>
+    </form>
   );
 }

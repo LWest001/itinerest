6c6c35ab891eea2f5cef085b2538e7f6da480e0b
Leo Westebbe <lw@Leos-MacBook-Pro.local>
2024-09-22 12:41:31 -0400

    add GeocodeSearchResult type

--- global.types.ts
+++ global.types.ts
@@ -2,3 +2,16 @@ import { Database, Tables } from "./database.types";
 
 export type Trip = Tables<'trips'>
 export type Activity = Tables<'activities'>
+export type GeocodeSearchResult = {
+    place_id: string;
+    licence: string;
+    osm_type: string;
+    osm_id: string;
+    boundingbox: string[];
+    lat: string;
+    lon: string;
+    display_name: string;
+    class: string;
+    type: string;
+    importance: number;
+}

9199b76b63b5425fe33769e81bde959baf5cd240
Leo Westebbe <lw@Leos-MacBook-Pro.local>
2024-09-30 16:18:29 -0400

    * use tailwind dark: function to fix hydration issue with conditional css rendering

--- components/ui/dashboard/nav-links.tsx
+++ components/ui/dashboard/nav-links.tsx
@@ -20,15 +20,17 @@ export default function NavLinks() {
       {links.map((link) => {
         const isActive = pathname === link.href;
         const LinkIcon = link.icon;
-        const classNames = clsx({
-          "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-background p-3 text-sm font-medium  md:flex-none md:justify-start md:p-2 md:px-3 dark:hover:bg-slate-800 dark:hover:text-inherit hover:bg-sky-100 hover:text-blue-600":
-            true,
-          "dark:bg-slate-800 dark:text-inherit bg-sky-100 text-blue-600":
-            isActive,
-        });
 
         return (
-          <Link key={link.name} href={link.href} className={classNames}>
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx({
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-background p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 hover:bg-secondary transition-colors dark:hover:text-inherit hover:text-blue-600":
+                true,
+              "bg-secondary": isActive,
+            })}
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>

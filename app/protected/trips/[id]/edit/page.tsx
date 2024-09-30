e09532bae0c1837f02299cd0f484e370a50c2db5
Leo Westebbe <lw@Leos-MacBook-Pro.local>
2024-09-30 16:20:23 -0400

    discard button on edit page goes back to the trip view page

--- app/protected/trips/[id]/edit/page.tsx
+++ app/protected/trips/[id]/edit/page.tsx
@@ -32,48 +32,46 @@ export default async function EditTrip({ params, searchParams }: Props) {
   const deleteTripWithId = deleteTrip.bind(null, id);
   const searchResults = await searchLocation(searchParams?.search);
 
-  return (
-    <div>
-      {trip && (
-        <form
-          className="flex w-full max-w-sm items-center gap-5 flex-col my-6"
-          action={updateTripWithId}
-        >
-          <FormFields
-            trip={trip}
-            formType="edit"
-            searchResults={searchResults}
-          />
-        </form>
-      )}
-      <div className="flex gap-2 items-center justify-center w-full max-w-sm">
-        <Button asChild variant={"ghost"}>
-          <Link href={"/protected/trips"}>Discard changes</Link>
-        </Button>
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant={"destructive"}>Delete trip</Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>
-                Are you sure you want to permanently delete this trip?
-              </AlertDialogTitle>
-              <AlertDialogDescription>
-                This cannot be undone.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction asChild>
-                <form action={deleteTripWithId}>
-                  <Button>Continue</Button>
-                </form>
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </div>
+console.log({ searchResults });
+
+return (
+  <div>
+    {trip && (
+      <form
+        className="flex w-full max-w-sm items-center gap-5 flex-col my-6"
+        action={updateTripWithId}
+      >
+        <FormFields trip={trip} formType="edit" searchResults={searchResults} />
+      </form>
+    )}
+    <div className="flex gap-2 items-center justify-center w-full max-w-sm">
+      <Button asChild variant={"ghost"}>
+        <Link href={`/a/trips/${id}`}>Discard changes</Link>
+      </Button>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant={"destructive"}>Delete trip</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Are you sure you want to permanently delete this trip?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              This cannot be undone.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <form action={deleteTripWithId}>
+                <Button>Continue</Button>
+              </form>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
-  );
+  </div>
+);
 }
